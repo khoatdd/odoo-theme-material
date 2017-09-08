723546fbd24db21912741969dad224e0258edac8
legenddotavn <dangkhoa114@gmail.com>
2017-09-09 06:52:44 +0700

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name' : 'Material theme',
+    'description': 'Material design base on Bootstrap 3',
+    'version': '1.0',
+    'author': 'Khoa',
+
+    'data' : [
+    ],
+    'category': 'Theme/Creative',
+    'depends': ['website'],
+    'application': True,
+    'installable': True,
+}
